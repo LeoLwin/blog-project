5e2ab461bbea0c4146d260f2f50a40e7310b9610
kaunghtetlwin982-bit <kaunghtetlwin@gmail.com>
2026-09-01 13:56:07 +0630

    fix express vercel deployment

--- server.js
+++ server.js
@@ -22,7 +22,17 @@ app.use('/static', express.static(path.join(import.meta.dirname)));
 const PORT = 5000;
 
 
-app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server running on ${PORT}`);
+// });
 
+
+if (process.env.NODE_ENV !== "production") {
+    const PORT = 5000;
+
+    app.listen(PORT, () => {
+        console.log(`Server running on ${PORT}`);
+    });
+}
+
+export default app;
