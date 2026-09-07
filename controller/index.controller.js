5392c9bebac919d113970e45da95d029b820481e
kaunghtetlwin982-bit <kaunghtetlwin@gmail.com>
2026-09-07 21:24:29 +0630

    update

--- controller/index.controller.js
+++ controller/index.controller.js
@@ -2,12 +2,13 @@ import { Router } from "express";
 import  blog from "./blog.controller.js";
 import user from "./user.controller.js";
 import auth from "./auth.controller.js";
+import authMiddleware from "../middleware/auth.middleware.js";
 const router = Router();
 
 router.get("/index",(req,res)=>{
     res.json('Index API is working fine');
 })
-router.use("/blogs", blog);
+router.use("/blogs",authMiddleware, blog);
 router.use("/users",user);
 router.use("/auth",auth);
 export default router;
